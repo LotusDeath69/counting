823160de9f91a909f5a57a8aa70a367cbad89aad
T. Hoang <LotusDeath69@gmail.com>
2021-05-25 10:56:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,9 @@ import discord
 from discord.ext import commands
 from replit import db 
 import os 
-from flask import Flask
-from threading import Thread
+from server import keepAlive
 prefix = '%'
 
-app = Flask('')
-
-@app.route('/')
-def home():
-  return 'success!'
-
-def run():
-  app.run(host='0.0.0.0', port=8080)
-
-def keepAlive():
-  t = Thread(target=run)
-  t.start()
 
 client = commands.Bot(command_prefix=prefix, help_command=None)
 
@@ -29,35 +16,31 @@ async def on_ready():
   print('logged in')
 
 
-@client.command()
-async def count(ctx, arg):
-  counting_channel_name = 'counting'
-  guild = ctx.message.guild.id
-  if arg.lower() == 'reset':
-    db[f'{guild}_counting_number'] = 1
-    await ctx.reply('Sucessfully reseted counting number.')
-  
-  if arg.lower() == 'help':
-    await ctx.reply('How to use: \nCreate a channel call counting\nDo %count <number> to count\nDo %count reset to rest the number.')
-  try:
-    int(arg) 
-    if str(ctx.message.channel.name) == counting_channel_name: 
-      try:
-        current_number = db[f'{guild}_counting_number'] 
-      except KeyError:
-        current_number = 1
-        db[f'{guild}_counting_number'] = 1
+@client.event
+async def on_message(message):
 
-      if str(arg) != str(current_number):
-        await ctx.send('Incorrect number. :clown:')
-      else:
-        await ctx.send(current_number)
-        await ctx.message.delete()
-        db[f'{guild}_counting_number'] = current_number + 1
+  if message.channel.name == 'counting' and message.author.bot == False:
+    number = message.content
+    try:
+      guild = message.guild.id
+      count_number = db[f'{guild}_count_number']
+    except KeyError:
+      count_number = 1
+    
+    if str(number) == str(count_number):
+      await message.add_reaction('\u2705')
+      db[f'{guild}_count_number'] = count_number + 1
+    elif message.content.lower() == 'count reset' and message.author.guild_permissions.administrator == True:
+      db[f'{guild}_count_number'] = 1
+      await message.reply('sucessfully reset the counting number.')
+    
     else:
-      await ctx.reply(f'Wrong channel go to #{counting_channel_name} :clown:')
-  except ValueError:
-    pass
+      await message.delete()
+      
+  
+@client.command()
+async def test(ctx, arg):
+  await ctx.reply('hello world')
 
-keepAlive()# only works if use Replit. Keep the bot running. 
+keepAlive()
 client.run(os.environ['TOKEN'])
